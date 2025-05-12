a4792c477c579b90667d711424cdd001a807fdc1
vr-oj <lay_genial_07@icloud.com>
2025-05-12 19:44:31 -0400

    version update

--- src/vasoanalyzer/version_checker.py
+++ src/vasoanalyzer/version_checker.py
@@ -1,8 +1,8 @@
 import requests
 
-def check_for_new_version(current_version="v2.5.1"):
+def check_for_new_version(current_version="v2.6"):
     try:
-        response = requests.get("https://api.github.com/repos/vr-oj/VasoAnalyzer_2.0/releases/latest")
+        response = requests.get("https://api.github.com/repos/vr-oj/VasoAnalyzer/releases/latest")
         if response.status_code == 200:
             latest_version = response.json().get("tag_name", "")
             if latest_version and latest_version != current_version:
