7833ca44393b820d6161f3c747ca1b974c660e91
Valdo Vega Rodríguez <lay_genial_07@icloud.com>
2025-06-11 19:27:12 -0400

    Fix event table column order

--- tests/test_update_plot.py
+++ tests/test_update_plot.py
@@ -21,3 +21,28 @@ def test_update_plot_no_frame(tmp_path):
     gui.update_plot()
     app.quit()
 
+
+def test_event_table_id_values(tmp_path):
+    os.environ.setdefault('QT_QPA_PLATFORM', 'offscreen')
+    trace_path = tmp_path / 'trace.csv'
+    df_trace = pd.DataFrame(
+        {
+            'Time (s)': [0, 1, 2, 3],
+            'Inner Diameter': [10, 11, 12, 13],
+        }
+    )
+    df_trace.to_csv(trace_path, index=False)
+
+    event_path = tmp_path / 'trace_table.csv'
+    df_evt = pd.DataFrame({'label': ['A', 'B'], 'time': [1, 2]})
+    df_evt.to_csv(event_path, index=False)
+
+    app = QApplication.instance() or QApplication([])
+    gui = VasoAnalyzerApp()
+    gui.load_trace_and_events(str(trace_path))
+
+    # Expected diameters at event times 1 and 2 seconds
+    assert gui.event_table_data[0][2] == 11.0
+    assert gui.event_table_data[1][2] == 12.0
+    app.quit()
+
