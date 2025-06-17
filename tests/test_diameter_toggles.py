07976529d3e2a42485c624f36a5a5d96fef5de5d
Valdo Vega Rodríguez <lay_genial_07@icloud.com>
2025-06-16 21:05:51 -0400

    Add diameter visibility toggles

---
+++ tests/test_diameter_toggles.py
@@ -0,0 +1,41 @@
+import os
+import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
+from PyQt5.QtWidgets import QApplication
+from vasoanalyzer.ui.main_window import VasoAnalyzerApp
+
+
+def test_diameter_toggle_visibility(tmp_path):
+    os.environ.setdefault('QT_QPA_PLATFORM', 'offscreen')
+    trace_path = tmp_path / 'trace.csv'
+    df = pd.DataFrame({
+        'Time (s)': [0, 1, 2],
+        'Inner Diameter': [10, 11, 12],
+        'Outer Diameter': [15, 16, 17],
+    })
+    df.to_csv(trace_path, index=False)
+    event_path = tmp_path / 'trace_table.csv'
+    pd.DataFrame({'label': ['A'], 'time': [1]}).to_csv(event_path, index=False)
+
+    app = QApplication.instance() or QApplication([])
+    gui = VasoAnalyzerApp()
+    gui.load_trace_and_events(str(trace_path))
+
+    assert gui.trace_line.get_visible() is True
+    assert gui.od_line.get_visible() is True
+
+    gui.toggle_inner_diameter(False)
+    gui.toggle_outer_diameter(False)
+
+    assert gui.trace_line.get_visible() is False
+    assert gui.od_line.get_visible() is False
+    assert gui.ax2.get_visible() is False
+
+    gui.toggle_inner_diameter(True)
+    gui.toggle_outer_diameter(True)
+
+    assert gui.trace_line.get_visible() is True
+    assert gui.od_line.get_visible() is True
+    assert gui.ax2.get_visible() is True
+    app.quit()
